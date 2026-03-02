d8de7618b35342a195556405af2fca688c7a36fd
matthael <mfponte2006@gmail.com>
2026-03-02 02:40:25 -0300

    add express

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 require('dotenv').config()
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot está online!");
+});
+
+app.listen(process.env.PORT || 3000);
+
 const { 
   Client, 
   GatewayIntentBits 
